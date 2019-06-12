7e7f2bec1e4467ba9fb78772c3c0bfa2ddf6a881
jacobdurst <jdd2007@gmail.com>
2019-06-12 00:05:39 -0400

    create app, delete some unncessary data

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Friends App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
